bbd8d204bba720241ba28e8554f17f026ea6592b
DormentCoder <29092866@students.lincoln.ac.uk>
2024-12-05 13:57:43 +0000

    Add files via upload

---
+++ Number Chooser.py
@@ -0,0 +1,7 @@
+#Using selection statements to check variables
+#Ask user for the number
+number = int(input("Enter a number 1-3:"))
+#Check the value of the number
+if number == 1: print("Number one")
+if number == 2: print("Number two")
+if number == 3: print("Number three")
